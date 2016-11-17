a5ed2bb1d4cbaeec8e653ff5e9c4983fe396cfd1
Ion Fong Chan <iochan@ucsc.edu>
2016-11-16 17:40:28 -0800

    Initial database table

--- models/tables.py
+++ models/tables.py
@@ -9,18 +9,42 @@
 
 import datetime
 
-db.define_table('checklist',
-                Field('user_email', default=auth.user.email if auth.user_id else None),
-                Field('title'),
-                Field('checklist', 'text'),
-                Field('last_opened', 'datetime', update=datetime.datetime.utcnow())
+db.define_table('categories',
+                Field('name', 'string'),
+                Field('description', 'string')
                 )
 
-# I don't want to display the user email by default in all forms.
-db.checklist.id.readable = db.checklist.id.writable = False
-db.checklist.user_email.readable = db.checklist.user_email.writable = False
-db.checklist.last_opened.readable = db.checklist.last_opened.writable = False
-db.checklist.title.requires = IS_NOT_EMPTY()
+db.define_table('recipe',
+                Field('user_id', 'reference auth_user', default=session.suth.user.id if session.auth else None),
+                Field('username', default=session.auth.user.first_name if session.auth else None),
+                Field('eating_style'),
+                Field('meal_type'),
+                Field('name', 'string'),
+                Field('description', 'string'),
+                Field('prep', 'time'),
+                Field('cook', 'time'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
+                Field('updated_on', 'datetime', default=datetime.datetime.utcnow()),
+                Field('vlink', 'text', default=None)
+                )
 
-# after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+db.define_table('ingredient',
+                Field('recipe_id', 'reference recipe', requires=IS_IN_DB(db, 'recipe.id')),
+                Field('material', 'text'),
+                Field('amount', 'double'),
+                Field('unit', 'string')
+                )
+
+# recipe table's constraint
+db.recipe.user_id.readable = db.recipe.user_id.writable = False
+db.recipe.username.readable = db.recipe.username.writable = False
+db.recipe.eating_style.requires = IS_IN_SET('Omnivore', 'Vegetarian', 'Vegan')
+db.recipe.meal_type.requires = IS_IN_SET('Appetizers & Snacks',
+                                         'Breakfast & Brunch',
+                                         'Lunch & Dinner',
+                                         'Dessert',
+                                         'Drinks')
+db.recipe.name.requires = IS_NOT_EMPTY()
+db.recipe.created_on.readable = db.recipe.created_on.writable = False
+db.recipe.updated_on.readable = db.recipe.updated_on.writable = False
+
