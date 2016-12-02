fbb891e35b35b1ca4c71a489239a7989d910260c
Rory Chan <rorycif@gmail.com>
2016-12-01 17:56:31 -0800

    Update database

--- models/tables.py
+++ models/tables.py
@@ -8,43 +8,53 @@
 # Consult manual for more options, validators, etc.
 
 import datetime
+db.define_table('cuisines',
+                Field('name'))
+
+db.define_table('mealType',
+                Field('name'))
+
+# insert data cuisines table
+if db(db.cuisines.id > 0).isempty():
+    db.cuisines.insert(name='African')
+    db.cuisines.insert(name='American')
+    db.cuisines.insert(name='Asian')
+    db.cuisines.insert(name='Australian')
+    db.cuisines.insert(name='European')
+    db.cuisines.insert(name='Indian')
+    db.cuisines.insert(name='Latin')
+    db.cuisines.insert(name='Polynesian')
+
+# insert data to mealType table
+if db(db.mealType.id > 0).isempty():
+    db.mealType.insert(name='Breakfast & Branch')
+    db.mealType.insert(name='Lunch')
+    db.mealType.insert(name='Dinner')
+    db.mealType.insert(name='Deserts')
+    db.mealType.insert(name='Appetizers')
+    db.mealType.insert(name='Snacks')
 
 db.define_table('recipe',
                 Field('user_id', 'reference auth_user', default=session.auth.user.id if session.auth else None),
                 Field('cuisines'),
-                Field('meal_type'),
+                Field('mealType'),
                 Field('name', 'string'),
                 Field('description', 'text'),
                 Field('ingredient', 'text'),
-                Field('prept', 'time', default='00:00:00'),
-                Field('cookt', 'time', default='00:00:00'),
+                Field('prept', 'integer', default=0),
+                Field('cookt', 'integer', default=0),
                 Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
                 Field('updated_on', 'datetime', default=datetime.datetime.utcnow()),
                 Field('vlink', 'text'),
                 )
 
 # Constraint for recipe
+db.recipe.name.requires = IS_NOT_EMPTY()
+db.recipe.cuisines.requires = IS_IN_DB(db, 'cuisines', '%(name)s')
+db.recipe.mealType.requires = IS_IN_DB(db, 'mealType', '%(name)s')
 db.recipe.id.readable = db.recipe.id.writable = False
 db.recipe.user_id.readable = db.recipe.user_id.writable = False
-db.recipe.name.requires = IS_NOT_EMPTY()
-db.recipe.created_on.writable = False
-db.recipe.created_on.writable = False
-
-db.recipe.cuisines.requires = IS_IN_SET(['Asian',
-                                         'African',
-                                         'European',
-                                         'American',
-                                         'Latin',
-                                         'Indian',
-                                         'Australian',
-                                         'Polynesian'])
-
-db.recipe.meal_type.requires = IS_IN_SET(['Breakfast & Brunch',
-                                          'Lunch',
-                                          'Dinner',
-                                          'Deserts',
-                                          'Appetizers',
-                                          'Snacks'], multiple=True)
-
+db.recipe.created_on.writable = db.recipe.created_on.readable = False
+db.recipe.updated_on.writable = db.recipe.updated_on.readable = False
 
 
